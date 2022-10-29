20df346a67645b71485ad86b55502d458594c259
Tony Brix <tony@brix.ninja>
2022-10-29 01:27:57 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -2,17 +2,21 @@ export default [
   {
     input: 'src/index.js',
     output: {
-      name: '|thisExtension|',
+      name: 'markedBidi',
       file: 'lib/index.umd.js',
-      format: 'umd'
-    }
+      format: 'umd',
+      globals: {
+        marked: 'marked'
+      }
+    },
+    external: ['marked']
   },
   {
     input: 'src/index.js',
     output: {
       file: 'lib/index.cjs',
       format: 'cjs'
-    }
+    },
+    external: ['marked']
   }
 ];
-
