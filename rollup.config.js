a197c2a384d5fa3ab002c81f1a2f675b52921e63
Tony Brix <tony@brix.ninja>
2022-10-29 01:15:27 -0500

    Update and rename rollup.config.umd.js to rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+export default [
+  {
+    input: 'src/index.js',
+    output: {
+      name: '|thisExtension|',
+      file: 'lib/index.umd.js',
+      format: 'umd'
+    }
+  },
+  {
+    input: 'src/index.js',
+    output: {
+      file: 'lib/index.cjs',
+      format: 'cjs'
+    }
+  }
+];
+
--- rollup.config.umd.js
+++
@@ -1,8 +0,0 @@
-export default {
-  input: 'src/index.js',
-  output: {
-    name: '|thisExtension|',
-    file: 'lib/index.umd.js',
-    format: 'umd'
-  }
-};
