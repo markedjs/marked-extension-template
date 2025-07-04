622b5eeddb2294c682c3a4fe3161d58c20e0ce91
Tony Brix <tony@brix.ninja>
2025-07-03 23:06:19 -0600

    remove console.log from test

--- spec/umd.test.js
+++ spec/umd.test.js
@@ -2,8 +2,6 @@ import { describe, test } from 'node:test';
 import { Marked } from 'marked';
 import '../lib/index.umd.js';
 
-console.log(globalThis.markedThisExtension);
-
 describe('this-extension', () => {
   test('no options', (t) => {
     const marked = new Marked();
