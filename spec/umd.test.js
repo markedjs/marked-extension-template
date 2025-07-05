dd16d95a6bbba46dd2f8df69ceb4367019fd8902
Tony Brix <tony@brix.ninja>
2025-07-05 17:44:37 -0600

    simplify umd test

--- spec/umd.test.js
+++ spec/umd.test.js
@@ -1,11 +1,8 @@
 import { describe, test } from 'node:test';
-import { Marked } from 'marked';
 import '../lib/index.umd.js';
 
 describe('marked-extension-template umd', () => {
-  test('no options', (t) => {
-    const marked = new Marked();
-    marked.use(globalThis.markedExtensionTemplate());
-    t.assert.snapshot(marked.parse('example markdown'));
+  test('test umd global', (t) => {
+    t.assert.equal(typeof markedExtensionTemplate, 'function');
   });
 });
